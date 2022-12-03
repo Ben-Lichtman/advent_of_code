1a4baeae19e5b2bdb1f51637babf36cff195bf3d
Ben-Lichtman <Ben_Lichtman@icloud.com>
2022-12-02 21:25:55 -0800

    refactored common part out

--- src/aoc2022/day3.rs
+++ src/aoc2022/day3.rs
@@ -1,25 +1,20 @@
 use std::{collections::HashSet, fmt::Display};
 
+fn map_string(i: &str) -> HashSet<u8> {
+	i.bytes()
+		.map(|b| match b {
+			b'a'..=b'z' => b - b'a' + 1,
+			b'A'..=b'Z' => b - b'A' + 27,
+			_ => panic!(),
+		})
+		.collect()
+}
+
 pub fn part1(i: &str) -> impl Display {
 	i.split('\n')
 		.map(|line| {
 			let (a, b) = line.split_at(line.len() / 2);
-			let (a, b) = (
-				a.bytes()
-					.map(|b| match b {
-						b'a'..=b'z' => b - b'a' + 1,
-						b'A'..=b'Z' => b - b'A' + 27,
-						_ => panic!(),
-					})
-					.collect::<HashSet<_>>(),
-				b.bytes()
-					.map(|b| match b {
-						b'a'..=b'z' => b - b'a' + 1,
-						b'A'..=b'Z' => b - b'A' + 27,
-						_ => panic!(),
-					})
-					.collect::<HashSet<_>>(),
-			);
+			let (a, b) = (map_string(a), map_string(b));
 			let common = &a & &b;
 
 			common.into_iter().next().unwrap() as i64
@@ -30,32 +25,11 @@ pub fn part1(i: &str) -> impl Display {
 pub fn part2(i: &str) -> impl Display {
 	i.split('\n')
 		.array_chunks::<3>()
-		.map(|line| {
+		.map(|group| {
 			let (a, b, c) = (
-				line[0]
-					.bytes()
-					.map(|b| match b {
-						b'a'..=b'z' => b - b'a' + 1,
-						b'A'..=b'Z' => b - b'A' + 27,
-						_ => panic!(),
-					})
-					.collect::<HashSet<_>>(),
-				line[1]
-					.bytes()
-					.map(|b| match b {
-						b'a'..=b'z' => b - b'a' + 1,
-						b'A'..=b'Z' => b - b'A' + 27,
-						_ => panic!(),
-					})
-					.collect::<HashSet<_>>(),
-				line[2]
-					.bytes()
-					.map(|b| match b {
-						b'a'..=b'z' => b - b'a' + 1,
-						b'A'..=b'Z' => b - b'A' + 27,
-						_ => panic!(),
-					})
-					.collect::<HashSet<_>>(),
+				map_string(group[0]),
+				map_string(group[1]),
+				map_string(group[2]),
 			);
 			let common = &(&a & &b) & &c;
 
