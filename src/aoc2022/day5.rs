e923924b8755186049d1dc90998b80345e88dd3a
Ben-Lichtman <Ben_Lichtman@icloud.com>
2022-12-04 21:54:58 -0800

    more refactor

--- src/aoc2022/day5.rs
+++ src/aoc2022/day5.rs
@@ -22,6 +22,17 @@ fn process_board(i: &str) -> Vec<Vec<char>> {
 	cols
 }
 
+pub fn get_from_to<T>(i: &mut [T], from: usize, to: usize) -> (&mut T, &mut T) {
+	if from < to {
+		let (left, right) = i.split_at_mut(to);
+		(&mut left[from], &mut right[0])
+	}
+	else {
+		let (left, right) = i.split_at_mut(from);
+		(&mut right[0], &mut left[to])
+	}
+}
+
 pub fn part1(i: &str) -> impl Display {
 	let (starter, instructions) = i.split_once("\n\n").unwrap();
 
@@ -37,16 +48,7 @@ pub fn part1(i: &str) -> impl Display {
 				.unwrap()
 		})
 		.for_each(|[count, from, to]: [_; 3]| {
-			let from = from - 1;
-			let to = to - 1;
-			let (col_from, col_to) = if from < to {
-				let (left, right) = cols.split_at_mut(to);
-				(&mut left[from], &mut right[0])
-			}
-			else {
-				let (left, right) = cols.split_at_mut(from);
-				(&mut right[0], &mut left[to])
-			};
+			let (col_from, col_to) = get_from_to(&mut cols, from - 1, to - 1);
 
 			let from_end = &col_from[col_from.len() - count..];
 			col_to.extend(from_end.iter().rev());
@@ -71,16 +73,7 @@ pub fn part2(i: &str) -> impl Display {
 				.unwrap()
 		})
 		.for_each(|[count, from, to]: [_; 3]| {
-			let from = from - 1;
-			let to = to - 1;
-			let (col_from, col_to) = if from < to {
-				let (left, right) = cols.split_at_mut(to);
-				(&mut left[from], &mut right[0])
-			}
-			else {
-				let (left, right) = cols.split_at_mut(from);
-				(&mut right[0], &mut left[to])
-			};
+			let (col_from, col_to) = get_from_to(&mut cols, from - 1, to - 1);
 
 			let from_end = &col_from[col_from.len() - count..];
 			col_to.extend(from_end.iter());
